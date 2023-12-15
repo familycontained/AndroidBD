139200bb26488bac83f9ffc610e94d736edfb44e
danitourer <danieltorresurena@gmail.com>
2023-12-15 20:39:11 +0100

    Tablas varias

--- BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuarios.java
+++ BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuarios.java
@@ -6,7 +6,10 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class Usuarios extends SQLiteOpenHelper {
 
-    String sqlCreate = "CREATE TABLE IF NOT EXISTS Usuarios (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
+    String sqlCreateUsuarios = "CREATE TABLE IF NOT EXISTS Usuarios (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
+    String sqlCreateTareas = "CREATE TABLE IF NOT EXISTS Tareas (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
+    String sqlCreateUsTar = "CREATE TABLE IF NOT EXISTS Usuario_Tarea (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";String sqlCreate = "CREATE TABLE IF NOT EXISTS Usuarios (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
+    String sqlCreateGrupos = "CREATE TABLE IF NOT EXISTS Grupos (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
 
     public Usuarios(Context contexto, String nombre, SQLiteDatabase.CursorFactory factory, int version) {
         super(contexto, nombre, factory, version);
