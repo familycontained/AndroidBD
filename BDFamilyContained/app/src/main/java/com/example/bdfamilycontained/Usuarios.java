3f518a39840f471ed01c006922aa8314acf388ce
danitourer <danieltorresurena@gmail.com>
2023-12-15 20:14:10 +0100

    Primer commit

---
+++ BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuarios.java
@@ -0,0 +1,7 @@
+package com.example.bdfamilycontained;
+
+public class Usuarios {
+
+    String sqlCreate = "CREATE TABLE Usuarios (id NUMERIC, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
+
+}
