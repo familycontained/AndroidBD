080ecaec880375ec28ab5049a587787df10b2644
Roberto Campo <116102679+AlexSerjs@users.noreply.github.com>
2023-12-21 16:19:28 +0100

    update xml y main

--- BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/MainActivity.java
+++ BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/MainActivity.java
@@ -4,40 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import com.example.bdfamilycontained.Usuarios; // Asegúrate de importar la clase Usuarios con el paquete correcto
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etNombre, etCorreo, etEdad;
+    private Button btnAgregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Usuarios usdbh = new Usuarios(this, "DBFamilyContained", null, 1); // Ajusta el nombre de la base de datos
-        SQLiteDatabase db = usdbh.getWritableDatabase();
+        Usuarios usdbh = new Usuarios(this, "DBFamilyContained", null, 1);
+        final SQLiteDatabase db = usdbh.getWritableDatabase();
 
-        // Si hemos abierto correctamente la base de datos
-        if (db != null) {
-            // Insertamos 5 usuarios de ejemplo
-            for (int i = 1; i <= 5; i++) {
+        etNombre = findViewById(R.id.etNombre);
+        etCorreo = findViewById(R.id.etCorreo);
+        etEdad = findViewById(R.id.etEdad);
+        btnAgregar = findViewById(R.id.btnAgregar);
 
-                // Generamos los datos
-                String nombre = "Usuario" + i;
-                String correo = "correo" + i + "@example.com";
-                int edad = 25 + i;
+        btnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Obtener los valores ingresados por el usuario
+                String nombre = etNombre.getText().toString();
+                String correo = etCorreo.getText().toString();
+                int edad = Integer.parseInt(etEdad.getText().toString());
 
-                // Utilizamos ContentValues para insertar los datos de manera segura
+                // Utilizar ContentValues para insertar los datos en la tabla Usuarios
                 ContentValues values = new ContentValues();
                 values.put("nombre", nombre);
                 values.put("correo_electronico", correo);
                 values.put("edad", edad);
 
-                // Insertamos los datos en la tabla Usuarios
+                // Insertar los datos en la tabla Usuarios
                 db.insert("Usuarios", null, values);
-            }
 
-            // Cerramos la base de datos
-            db.close();
-        }
+                // Limpiar los campos después de la inserción
+                etNombre.setText("");
+                etCorreo.setText("");
+                etEdad.setText("");
+            }
+        });
     }
 }
