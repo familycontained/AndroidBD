fe12a00c4f09378e9b2306d57046c2f8338d3dff
danitourer <danieltorresurena@gmail.com>
2024-01-11 15:56:48 +0100

    tareas

--- BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuarios.java
+++ BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuarios.java
@@ -9,7 +9,7 @@ public class Usuarios extends SQLiteOpenHelper {
     String sqlCreateUsuarios = "CREATE TABLE IF NOT EXISTS Usuarios (idUsuarios INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, correo_electronico TEXT, edad NUMERIC)";
     String sqlCreateTareas = "CREATE TABLE IF NOT EXISTS Tareas ( IDTarea INTEGER PRIMARY KEY AUTOINCREMENT, Descripcion TEXT,FechaCreacion DATE, FechaRealizacion DATE,Estado TEXT,IDUsuario INTEGER)";
     String sqlCreateUsTar = "CREATE TABLE IF NOT EXISTS Usuario_Tarea (  IDRelacion INTEGER PRIMARY KEY AUTOINCREMENT,IDUsuario INTEGER, IDTarea INTEGER)";
-   String sqlCreateGrupos = "CREATE TABLE IF NOT EXISTS Grupos ( IDGrupo INTEGER PRIMARY KEY AUTOINCREMENT, NombreGrupo TEXT, DescripcionGrupo TEXT,IDCreador INTEGER )";
+    String sqlCreateGrupos = "CREATE TABLE IF NOT EXISTS Grupos ( IDGrupo INTEGER PRIMARY KEY AUTOINCREMENT, NombreGrupo TEXT, DescripcionGrupo TEXT,IDCreador INTEGER )";
 
     public Usuarios(Context contexto, String nombre, SQLiteDatabase.CursorFactory factory, int version) {
         super(contexto, nombre, factory, version);
