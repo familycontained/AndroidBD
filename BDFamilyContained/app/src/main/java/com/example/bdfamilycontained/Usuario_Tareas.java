3b4a22477185bcd432f9c9b39738c7b0cf35ddaa
danitourer <danieltorresurena@gmail.com>
2024-01-12 17:50:04 +0100

    us-tar

--- BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuario_Tareas.java
+++ BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Usuario_Tareas.java
@@ -1,4 +1,13 @@
 package com.example.bdfamilycontained;
 
-public class Usuario_Tareas {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Usuario_Tareas extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_usuario_tarea); // Usa el layout correspondiente
+    }
 }
