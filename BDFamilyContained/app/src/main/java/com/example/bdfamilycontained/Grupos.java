e1a7579ed1bde81ad5fe8fe31f14e368e87f3d1a
AlexSerjs <alexserjs@gmail.com>
2024-01-12 17:49:30 +0100

    Actividad03

--- BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Grupos.java
+++ BDFamilyContained/app/src/main/java/com/example/bdfamilycontained/Grupos.java
@@ -1,4 +1,12 @@
 package com.example.bdfamilycontained;
 
-public class Grupos {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Grupos extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_grupos); // Usa el layout correspondiente
+    }
 }
